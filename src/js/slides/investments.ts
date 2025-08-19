e0d0669c2200980ad1fcaec22f8b28f3959d4417
Artem Safin <artemsafin.webdev@gmail.com>
2025-08-19 22:36:54 +0300

    Анимация

--- src/js/slides/investments.ts
+++ src/js/slides/investments.ts
@@ -5,12 +5,89 @@ gsap.registerPlugin(SplitText);
 
 export default function investments(section: HTMLElement) {
   const heading = section.querySelector(".investments__heading");
+  const bgImage = section.querySelector(
+    ".investments__bg-image"
+  ) as HTMLImageElement;
+  const bgContainer = section.querySelector(".investments__bg") as HTMLElement;
+  const counter = section.querySelector(".investments__count");
+  const targetValue = parseInt(counter?.textContent?.trim() || "0", 10);
+  const counterWrapper = section.querySelector(".investments__wrapper");
 
   const tl = gsap.timeline();
+  let headingSplit = SplitText.create(heading);
+  tl.fromTo(
+    bgImage,
+    {
+      y: 0,
+    },
+    {
+      y: () => {
+        const containerHeight = bgContainer.offsetHeight;
+        const imageHeight = bgImage.offsetHeight;
+        const heightDifference = imageHeight - containerHeight;
+        if (imageHeight <= containerHeight) return 0;
+        return heightDifference;
+      },
+      duration: 2,
+      ease: "power1.out",
+    }
+  );
 
-  tl.from(heading, {
-    autoAlpha: 0,
-    duration: 1,
-    y: 100,
-  });
+  tl.fromTo(
+    headingSplit.chars,
+    {
+      autoAlpha: 0,
+    },
+    {
+      duration: 0.4,
+      autoAlpha: 1,
+      stagger: 0.05,
+    },
+    "-=1"
+  );
+
+  // Counter animation from 0 to target value
+
+  tl.fromTo(
+    counter,
+    {
+      autoAlpha: 0,
+    },
+    {
+      autoAlpha: 1,
+      duration: 1,
+    },
+    "<+=1"
+  );
+
+  tl.fromTo(
+    counter,
+    {
+      innerText: 0,
+    },
+    {
+      innerText: targetValue,
+      duration: 2,
+      ease: "none",
+      snap: { innerText: 1 }, // Snap to whole numbers
+    },
+    "<" // Start counter animation 0.5 seconds before heading animation ends
+  );
+
+  tl.fromTo(
+    counterWrapper,
+    {
+      x: -30,
+      autoAlpha: 0,
+    },
+    {
+      x: 0,
+      autoAlpha: 1,
+      duration: 0.7,
+      ease: "power2.out",
+    },
+    "+=0.1"
+  );
+
+  return tl;
 }
--- src/js/slideshow.ts
+++ src/js/slideshow.ts
@@ -1,7 +1,8 @@
 import Swiper from "swiper";
 import "swiper/css";
-import { Autoplay, Keyboard, Mousewheel } from "swiper/modules";
+import { Autoplay, Keyboard, Mousewheel, EffectFade } from "swiper/modules";
 import investments from "./slides/investments";
+import "swiper/scss/effect-fade";
 
 export default function slideshow() {
   const elements = Array.from(
@@ -18,6 +19,7 @@ export default function slideshow() {
       if (!section?.hasAttribute("data-section-name")) return;
       const name = section.getAttribute("data-section-name");
       console.log("Section name", name);
+
       if (name === "investments") {
         investments(section);
       }
@@ -26,7 +28,11 @@ export default function slideshow() {
     const instance = new Swiper(container, {
       direction: "vertical",
       speed: 1000,
-      modules: [Keyboard, Mousewheel, Autoplay],
+      modules: [Keyboard, Mousewheel, Autoplay, EffectFade],
+      effect: "fade",
+      fadeEffect: {
+        crossFade: true,
+      },
       longSwipesRatio: 0.2,
       keyboard: {
         onlyInViewport: false,
